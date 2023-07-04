ea2db96a4cd54b484b15f1806dd1d2af3da960ac
Harshita Lakhchaura <96336647+harshita-lakhchaura@users.noreply.github.com>
2023-07-04 09:24:46 +0530

    Time: 9 ms (58.12%), Space: 10.2 MB (8.78%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n=nums.size();
-        int ans=0;
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++)
-            mp[nums[i]]++;
-        for(auto i:mp)
+        long long int n=nums.size();
+        long long int ans=0;
+        set<long long int>s;
+        long long int sum1=0;
+        for(long long int i=0;i<n;i++)
         {
-            if(i.second==1)
-                return i.first;
+            sum1+=nums[i];
+            s.insert(nums[i]);
         }
-        return -1;
+        long long int sum2=0;
+        for(auto i:s)
+            sum2+=i;
+        ans=((3*sum2)-sum1)/2;
+        return ans;
     }
 };
