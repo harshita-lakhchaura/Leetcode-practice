33c979f0b384432b3c1cc9ecf9ee15e66d887cbb
Harshita Lakhchaura <96336647+harshita-lakhchaura@users.noreply.github.com>
2023-12-20 08:56:57 +0530

    Time: 17 ms (25.91%), Space: 47.1 MB (29.77%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        int ans=INT_MAX;
+        
         sort(prices.begin(),prices.end());
         int n=prices.size();
-        int i=0,j=n-1;
-        while(i<j)
-        {
-            if(prices[i]+prices[j]>money)
-            {
-                j--;
-            }
-            else if(prices[i]+prices[j]<=money)
-            {
-                ans=money-(prices[i]+prices[j]);
-                j--;
-            }
-        }
+        
+        int mn1=*min_element(prices.begin(),prices.end());
+        
+        if(count(prices.begin(),prices.end(),mn1)==1)
+            prices.erase(remove(prices.begin(),prices.end(),mn1),prices.end());
+        
+        int mn2=*min_element(prices.begin(),prices.end());
+        
+        int ans=mn1+mn2;
+        
         if(ans>money)
             ans=money;
+        else
+            ans=money-ans;
         return ans;
     }
 };
