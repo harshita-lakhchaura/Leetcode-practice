104803d96f4d8a05399ed9b5d32fb602ab5920f6
Harshita Lakhchaura <96336647+harshita-lakhchaura@users.noreply.github.com>
2024-08-09 14:14:51 +0530

    Time: 19 ms (29.59%), Space: 19.5 MB (46.16%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n=nums.size();
-        int sum=0;
-        set<int>s;
-        for(int i=0;i<n;i++)
-        {
-            s.insert(nums[i]);
-            sum+=nums[i];
-        }
-        int sumUnique=0;
-        for(auto i:s)
-        {
-            sumUnique+=i;
-        }
-        int ans=2*sumUnique-sum;
+        int ans=0;
+        for(int i=0;i<nums.size();i++)
+            ans=ans^nums[i];
         return ans;
     }
 };
